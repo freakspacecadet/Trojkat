8fcc8151210fef1d2bf7274afa7d0485c7c006bf
freakspacecadet <125437248+freakspacecadet@users.noreply.github.com>
2023-03-14 14:17:46 +0100

    Add files via upload

---
+++ Trojkat-main/Trojkat-main/Trójkąt/Trójkąt/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trójkąt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double a;
+        double b;
+        double c;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            string funkcja = (string)comboBox2.SelectedItem;
+            string kat = (string)comboBox1.SelectedItem;
+            if (funkcja == "sin") {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / c)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / c)); }
+            }
+            else if (funkcja == "cos")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / c)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / c)); }
+            }
+            else if (funkcja == "tg")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / b)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / a)); }
+            }
+            else if (funkcja == "ctg")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / a)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / b)); }
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(numericUpDown1.Value);
+            c = Math.Sqrt(a * a + b * b);
+            textBox1.Text = Convert.ToString(String.Format("{0:0.000}", c));
+            string funkcja = (string)comboBox2.SelectedItem;
+            string kat = (string)comboBox1.SelectedItem;
+            if (funkcja == "sin")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / c)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / c)); }
+            }
+            else if (funkcja == "cos")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / c)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / c)); }
+            }
+            else if (funkcja == "tg")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / b)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / a)); }
+            }
+            else if (funkcja == "ctg")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / a)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / b)); }
+            }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            b = Convert.ToDouble(numericUpDown2.Value);
+            c = Math.Sqrt(a * a + b * b);
+            textBox1.Text = Convert.ToString(String.Format("{0:0.000}", c));
+            string funkcja = (string)comboBox2.SelectedItem;
+            string kat = (string)comboBox1.SelectedItem;
+            if (funkcja == "sin")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / c)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / c)); }
+            }
+            else if (funkcja == "cos")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / c)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / c)); }
+            }
+            else if (funkcja == "tg")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / b)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / a)); }
+            }
+            else if (funkcja == "ctg")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / a)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / b)); }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string funkcja = (string)comboBox2.SelectedItem;
+            string kat = (string)comboBox1.SelectedItem;
+            if (funkcja == "sin")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / c)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / c)); }
+            }
+            else if (funkcja == "cos")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / c)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / c)); }
+            }
+            else if (funkcja == "tg")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / b)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / a)); }
+            }
+            else if (funkcja == "ctg")
+            {
+                if (kat == "alfa") { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", b / a)); }
+                else { textBox2.Text = Convert.ToString(String.Format("{0:0.000}", a / b)); }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
